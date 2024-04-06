e9b150cca8d8af36e3bca8724d48ada1e590beef
muhammadahmedraza12 <ahmeedr916789@gmail.com>
2024-04-06 15:39:54 +0500

    Currency-Converter-Cli Task

---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let user_ans = await inquirer.prompt([
+    {
+        name: 'from',
+        message: "Enter from currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'to',
+        message: "Enter to currency",
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'amount',
+        message: "Enter your amount",
+        type: 'number',
+    }
+]);
+let userFromCurrency = user_ans.from;
+let userToCurrency = user_ans.to;
+let fromAmount = currency[userFromCurrency];
+let toAmount = currency[userToCurrency];
+let amount = user_ans.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
